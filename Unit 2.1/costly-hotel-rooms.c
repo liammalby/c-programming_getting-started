dd9285a9a78563a7756b6931e69017d98ab87b48
Liam Malby <8891240+liammalby@users.noreply.github.com>
2022-01-06 13:04:33 +1000

    nested if statements

--- Unit 2.1/costly-hotel-rooms.c
+++ Unit 2.1/costly-hotel-rooms.c
@@ -9,6 +9,14 @@ if you are less than 10 years old. For everyone else, the cost is 30 dollars
 plus an additional 10 dollars if you bring more than 20 pounds of luggage.
 Your program should read the customer's age first, then the weight of their
 luggage, then output the price they have to pay.
+
+Example input:
+22
+25
+
+Example output:
+40
+
 */
 
 #include <stdio.h>
@@ -18,4 +26,34 @@ int main(void)
     int custAge, weight;
     custAge = 0;
     weight = 0;
+
+    // Read in age and weight
+    scanf("%d\n%d", &custAge, &weight);
+
+    // One room costs nothing if you are 60
+    if(custAge == 60)
+    {
+        printf("0");
+    }
+    else
+    {
+        if(custAge < 10 && custAge != 0)
+        {
+            printf("5");
+        }
+        else
+        {
+            if(weight < 21)
+            {
+                printf("30");
+            }
+            else
+            {
+                printf("40");
+            }
+        }
+    }
+
+return 0;
+
 }
